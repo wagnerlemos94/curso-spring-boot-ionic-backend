ee9e160958047e3749bcd6ea2df3f73975baadc6
wagner lemos <wagner.lemos94@hotmail.com>
2020-06-28 17:00:26 -0300

    Validação cusntomizada de FPC ou CNPJ na inserção de Cliente

--- src/main/java/com/nelioalves/cursomc/domain/enuns/EstadoPagamento.java
+++ src/main/java/com/nelioalves/cursomc/domain/enuns/EstadoPagamento.java
@@ -1,16 +1,14 @@
 package com.nelioalves.cursomc.domain.enuns;
 
 public enum EstadoPagamento {
-	
-	PEDENTE(1, "pendente"),
-	QUITADO(2, "Quitado"),
-	CANCELADO(3, "Cancelado");
-	
+
+	PEDENTE(1, "pendente"), QUITADO(2, "Quitado"), CANCELADO(3, "Cancelado");
+
 	private int cod;
 	private String descricao;
-	
+
 	private EstadoPagamento(int cod, String descrica) {
-		this.cod =  cod;
+		this.cod = cod;
 		this.descricao = descrica;
 	}
 
@@ -21,14 +19,14 @@ public enum EstadoPagamento {
 	public String getDescricao() {
 		return descricao;
 	}
-	
+
 	public static EstadoPagamento toEnum(Integer cod) {
-		if(cod == null) {
+		if (cod == null) {
 			return null;
 		}
-		for(EstadoPagamento x : EstadoPagamento.values()) {
-			if(cod.equals(x.getCod())) {
-				return x;  
+		for (EstadoPagamento x : EstadoPagamento.values()) {
+			if (cod.equals(x.getCod())) {
+				return x;
 			}
 		}
 		throw new IllegalArgumentException("Id Inválido: " + cod);
--- src/main/java/com/nelioalves/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/nelioalves/cursomc/dto/ClienteDTO.java
@@ -18,7 +18,7 @@ public class ClienteDTO implements Serializable{
 	private String nome;
 	
 	@NotEmpty(message = "Preenchimento obrigatório")
-	@Email
+	@Email(message = "Email inválido")
 	private String email;
 	
 	public ClienteDTO() {
