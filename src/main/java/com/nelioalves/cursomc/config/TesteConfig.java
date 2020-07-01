a2c2d7fd491320ade0336db89b18dda582ea3ab8
wagner lemos <wagner.lemos94@hotmail.com>
2020-07-01 18:04:21 -0300

    MockEmailService com Logger. Padrões Strategy e Template Method

--- src/main/java/com/nelioalves/cursomc/config/TesteConfig.java
+++ src/main/java/com/nelioalves/cursomc/config/TesteConfig.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.nelioalves.cursomc.services.DBService;
+import com.nelioalves.cursomc.services.EmailService;
+import com.nelioalves.cursomc.services.MockEmailService;
 
 @Configuration
 @Profile("test")
@@ -21,6 +23,11 @@ public class TesteConfig {
 		dbService.instantiateDatabase();
 		
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new MockEmailService();
 	}
 
 }
