e49999b8412339d6280e4a3c2b876ee6708ccd3d
wagner lemos <wagner.lemos94@hotmail.com>
2020-07-01 20:17:13 -0300

    Email HTML

--- src/main/java/com/nelioalves/cursomc/services/MockEmailService.java
+++ src/main/java/com/nelioalves/cursomc/services/MockEmailService.java
@@ -1,5 +1,7 @@
 package com.nelioalves.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
@@ -12,6 +14,15 @@ public class MockEmailService extends AbstractEmailService{
 	public void sendEmail(SimpleMailMessage msg) {
 		
 		LOG.info("Simulando envio de email...");
+		LOG.info(msg.toString());
+		LOG.info("Email enviado");
+		
+	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		
+		LOG.info("Simulando envio de email HTML...");
 		LOG.info(msg.toString());
 		LOG.info("Email enviado");
 		
